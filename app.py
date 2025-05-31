1f4f6af272af80bfc7b28d88daad83a6613eea87
Aneesh Angane <anesh.angane@gmail.com>
2025-05-31 12:20:57 +0530

    changes

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-import os  # Import the os module to access environment variables
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
-
-
-if __name__ == "__main__":
-    # Get the port from the environment variable, default to 5000 for local development
-    port = int(os.environ.get("PORT", 5000))
-    app.run(
-        host="0.0.0.0", port=port, debug=False
-    )  # Set debug=False for production deployment
+def hello_world():
+    return "Hello, World!"
